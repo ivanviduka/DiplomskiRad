89e92e95bd6b9d71a0f0bc9e5aa0fa54948b4d88
ivanviduka <44037984+ivanviduka@users.noreply.github.com>
2022-05-03 17:49:15 +0200

    Optimization/code cleanup (#11)
    * Code cleanup
    
    * Fixed comment validation

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\CreateCommentRequest;
 use App\Models\Comment;
+use App\Models\File;
 
 class CommentController extends Controller
 {
     public function createComment(CreateCommentRequest $request)
     {
+        if(!File::find($request->file_id)->is_public){
+            redirect()->back()->withErrors(['not_public' => 'Cannot submit comment']);
+        }
+
         Comment::create([
             'comment_text' => $request->comment,
             'file_id' => $request->file_id,
